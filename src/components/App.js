228a2b97286ed288b305e648d9d16844c42cdde3
IvanWiesner <ivanwiesner@gmail.com>
2021-10-20 12:17:20 -0600

    t

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
     <div className="App">
       <p>test routes</p>
-      <div classname="routes">
+      <div classname="App-controllers">
       <Switch>
         <Route>
           <Controller/> 
